0fc444999adcc37a27a768a5f83cefc662cdb76e
Marc Simonton <marcasimonton@gmail.com>
2016-08-03 13:51:17 -0600

    returning-songs-and-names

--- main.js
+++ main.js
@@ -1,68 +1,48 @@
+$(document).ready(()=> {
+  var corsAnywhere="https://cors-anywhere.herokuapp.com/"
+  var urlSearch="http://api.lyricsnmusic.com/songs?api_key=a974cb46f8bfe28f1e58995879ee22&q="
+
+  $('form').submit(function(even){
+    event.preventDefault();
+    var searchItems = $('#main_search').val();
+    $.ajax({
+      url:corsAnywhere+urlSearch+searchItems,
+      error:function(err) {
+        console.log(err)
+      },
+      method:'GET',
+      success:function(searchResults)    {
+        console.log(searchResults)
+        for(var i=0; i<10;i++)  {
+          for(var j of i) {
+            if
+          }
+          $('#search_return').append('<h3>'+searchResults[i].title+'</h3>')
+          $('#search_return').append('<span>'+searchResults[i].artist.name+'</span>')
+
+        }
+      }
 
 
-    url:"http://api.edmunds.com/api/vehicle/v2/makes?fmt=json&api_key=jgv28njxfw5k75t4m5v23mdr",
-    error: function(err)  {
-      console.log(err)
-    },
-    method:'GET',
-    success: function(test) {
-      console.log(test)
-    }
+
+
+
+    })
+
+
   })
+
+
+
 })
-
-// for(var findModels of carList)  {
-//   $.ajax({
-//   url:"'https://api.edmunds.com/api/vehicle/v2/"+[findModels]+"/models?fmt=json&year=1995&api_key=jgv28njxfw5k75t4m5v23mdr",
-//   error: function(err)  {
-//     console.log(err)
-//   },
-//   method:'GET',
-//   success: function(carList) {
-//
-//
-// }
-
-
-//}
-      // for(var i=0; i<=makesObj.length;i++)  {
-      //   var blah=makesObj[i].niceName;
-      //
-      //   vehicleMakes.push(blah)
-      //   console.log(vehicleMakes)
-      // }
-
-      // var makeNames = test.makes;
-      // for(var name of test)  {
-      //   console.log(test)
-      //}
-    //}
-
-    //})
-
-//  })
-
-//dict.1.1.20160802T162404Z.cce2e7147a9af2e4.939092a875c910b9c74cbf3c45f44d4b10591110
-
-
-
-
-
-  // $(document).ready(()=> {
-  //     $.ajax({
-  //     url:"https://api.edmunds.com/api/vehicle/v2/honda/accord?fmt=json&state=used&api_key=jgv28njxfw5k75t4m5v23mdr",
-  //     error: function(err)  {
-  //       console.log(err)
-  //     },
-  //
-  //     method:'GET',
-  //     success: function(tester) {
-  //       console.log(tester)
-  //     }
-  //     })
-  //   })
 //https://api.edmunds.com/api/media/v2/{make}/{model}/{year}/photos?api_key=jgv28njxfw5k75t4m5v23mdr&fmt=json
 
 
+
+
+
+//a974cb46f8bfe28f1e58995879ee22
+
   //53z8gsmpx9yqy9fran5r8y44
   //jgv28njxfw5k75t4m5v23mdr
+//

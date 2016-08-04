2bba4050481889c181fdc15bfb3a8f1573219153
Marc Simonton <marcasimonton@gmail.com>
2016-08-04 11:56:10 -0600

    den-devs-day

--- main.js
+++ main.js
@@ -5,36 +5,45 @@ $(document).ready(()=> {
   $('form').submit(function(even){
     event.preventDefault();
     var searchItems = $('#main_search').val();
+
+
     $.ajax({
       url:corsAnywhere+urlSearch+searchItems,
       error:function(err) {
         console.log(err)
       },
+
+
       method:'GET',
       success:function(searchResults)    {
-        console.log(searchResults)
-        for(var i=0; i<10;i++)  {
-          for(var j of i) {
-            if
+        //for(var i=0; i<10;i++)
+        var removeDuplicates=[];
+        searchResults.sort(function(a,b)  {
+          for(var i=0; i<searchResults.length-1; i++) {
+            if(searchResults[i].artist.name == searchResults[i+1].artist.name && searchResults[i].title == searchResults[i+1].title)  {
+              delete searchResults[i]
+            }
           }
+          searchResults=searchResults.filter( function(el){
+            return(typeof el !=="undefined")
+          })
+      })
+      console.log(searchResults)
+
+          for(var i=0; i<10;i++){
           $('#search_return').append('<h3>'+searchResults[i].title+'</h3>')
-          $('#search_return').append('<span>'+searchResults[i].artist.name+'</span>')
-
+          $('#search_return').append('<span><strong>Artist:</strong> '+searchResults[i].artist.name+'</span>')
         }
-      }
-
-
-
-
-
+        $('h3').click(function()  {
+          this
+        })
+        }
+      })
     })
 
 
   })
 
-
-
-})
 //https://api.edmunds.com/api/media/v2/{make}/{model}/{year}/photos?api_key=jgv28njxfw5k75t4m5v23mdr&fmt=json
 
 
